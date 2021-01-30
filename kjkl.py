7cbe62ee664f0327321c6c645cbd3f9396285e19
rodrigo <chaboxx159@gmail.com>
2021-01-30 12:53:02 -0500

    asfasg

--- kjkl.py
+++ kjkl.py
@@ -1,5 +1,10 @@
-for i in range(4):
-    for j in range(5):
-        
-        for k in range(7):
-            print(i,j,k)
+class Persona():
+
+    def __init__(self,LO_QUE_TU_TE_DES):
+        super().__init__()
+        self.amor=LO_QUE_TU_TE_DES
+    
+    
+
+yo=Persona("Todo,todo mi amor , toda mi vida , todo lo que soy, todo lo que me hace feliz,todo lo que hace feliz a mi hermana , todo lo que el universo y mi mente quiere, amor amor amor amor para mi (rodrigo cueva pastor) , mi hermana (gladys) , mi padre , mi madre.")
+
